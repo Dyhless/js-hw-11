949d1c0ead7b6c77141e1d1c4234f22e183edb7c
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-03 19:15:17 +0200

    update id

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
 /*
-https://www.thecocktaildb.comapi.php
+https://www.thecocktaildb.com/api.php
 */
 
 const refs = {
   input: document.querySelector('#input'),
   form: document.querySelector('#form'),
-  button: document.querySelector('#button'),
+  button: document.querySelector('button'),
+  divDrinks: document.querySelector('.container'),
 }
 
 refs.form.addEventListener('submit', onFormSubmit);
@@ -13,8 +14,21 @@ refs.form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
   const inputValue = refs.input.value;
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${inputValue}`)
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`)
     .then(res => res.json())
-      .then(date => console.log(date))
+      .then(date => renderDrinks(date.drinks))
         .catch(error => console.log(error));
+}
+
+function drinkMarkup({ strDrinkThumb, strDrink }) {
+  const markup = 
+  `<div>
+    <img src = "${strDrinkThumb}" alt = "${strDrink}" width = "200">
+    <h1>${strDrink}</h1>
+  </div>`;
+  refs.divDrinks.insertAdjacentHTML('afterend', markup);
+}
+
+function renderDrinks(array) {
+  array.forEach(element => drinkMarkup(element));
 }
