03bf55986eda9141fd43a1875aadb2cbee9fe977
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-05 20:03:57 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import * as pixabayApi from './api.js';
 
