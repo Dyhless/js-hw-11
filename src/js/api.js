95379230248bd0e6092b132931f4cbafb222dcd8
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-05 20:57:50 +0200

    update

--- src/js/api.js
+++ src/js/api.js
@@ -1,2 +1,8 @@
 export const BASE_URL = 'https://pixabay.com/api/';
 export const API_KEY = '37053026-4150c05ae3a340932daa6308e';
+
+export function getPictures(query) {
+   fetch('${BASE_URL}?apiKey=${API_KEY}&q=${query}')
+} 
+
+getPictures("gpt-3");
--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,8 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 import * as pixabayApi from './api.js';
+import { refs } from './refs.js';
+
 
