732708145ecee25a6580db04817097eeaf710b34
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-05 19:21:16 +0200

    update

--- src/js/api.js
+++ src/js/api.js
@@ -1,22 +1,2 @@
-import axios from 'axios';
-import { BASE_URL, API_KEY } from './refs';
-
-export class FetchApi {
-  async getData(BASE_URL, API_KEY) { 
-    try {
-      const fetch = await axios
-        .get(BASE_URL, {
-          params: { apikey: API_KEY, limmit: '100' }
-        });
-      
-      return fetch.data.data.results;
-    }
-    catch (error) {
-      console.log(error.message);
-      return false;
-    } 
-  }
-}
-
-export const try1 = new FetchApi();
-try1.getData(BASE_URL + API_KEY);
+export const BASE_URL = 'https://pixabay.com/api/';
+export const API_KEY = '37053026-4150c05ae3a340932daa6308e';
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,3 @@
 import axios from 'axios';
-import { BASE_URL, API_KEY } from './refs';
-import { try1 } from './api';
-
-
-(async () => {
-  const date = await try1.getData(BASE_URL + API_KEY);
-  console.log(date);
-})();
+import * as pixabayApi from './api.js';
 
