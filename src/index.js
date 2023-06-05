121a0f5703a10f7ab819ed9b3597ede3443d85fe
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-05 22:27:38 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,8 +4,27 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { refs } from './js/refs.js';
-import * as pixabayApi from './js/api.js';
+import API from './js/api.js';
 
+console.log(API);
 
+/*
 
+1. Get refs 
+2. Create querySelector on submit 
+3. Get request form input and sent to server like query parametr
+4. Check the server response
+ 4.1 in case the nagative response, notify the user
+5. Get results and literate through the array and create the markup
+   (gether everything in one line)
+6. Show the markup to user (innerHTML)
+7. Clear the form    
 
+*/
+
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) { 
+   event.peventDefault();
+   console.log(event.currentTarget.elements);
+}
--- src/js/api.js
+++ src/js/api.js
@@ -1,10 +1,10 @@
-export const BASE_URL = 'https://pixabay.com/api/';
-export const API_KEY = '37053026-4150c05ae3a340932daa6308e';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '37053026-4150c05ae3a340932daa6308e';
 
-
-export async function getPictures(query) {
+async function getPictures(query) {
    const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}`);
    const result = await response.json();
    return console.log(result);
 } 
 
+export default { getPictures };
