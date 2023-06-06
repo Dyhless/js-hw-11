babfc58c7b49f5577e93744438789df27dfd376a
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-06 20:59:42 +0200

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -5,17 +5,16 @@ const API_KEY = '37053026-4150c05ae3a340932daa6308e';
 
 async function getPictures(query) {
    try {
-      const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}`);
+      const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`);
       if (!response.ok) {
-         throw new Error('Request failed with status ' + response.status);
+         throw new Error('Error with status' + response.status);
       }
       const result = await response.json();
       return result;
    } catch (error) {
-      console.error('Error:', error);
-      throw error; // rethrow the error to be caught in the `onError` function
+      console.error('Ошибка:', error);
+      throw error; 
    }
 }
 
 export default { getPictures };
-
