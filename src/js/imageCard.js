d60608129c9a6483a8c230d5cec38727e4482dfb
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-08 19:53:58 +0200

    add async

--- src/js/imageCard.js
+++ src/js/imageCard.js
@@ -1,21 +1,23 @@
-function openImage(imageURL) {
-   window.open(imageURL, '_blank');
-}
+const imageCard = {
+  async openImage(imageURL) {
+    window.open(imageURL, '_blank');
+  },
 
-function createImageCard(image) {
-   const card = document.createElement('div');
-   card.classList.add('card');
+  createImageCard(image) {
+    const card = document.createElement('div');
+    card.classList.add('card');
 
-   const imageElement = document.createElement('img');
-   imageElement.src = image.webformatURL;
-   imageElement.alt = image.tags;
+    const imageElement = document.createElement('img');
+    imageElement.src = image.webformatURL;
+    imageElement.alt = image.tags;
 
-   imageElement.addEventListener('click', () => {
-      openImage(image.largeImageURL);
-   });
+    imageElement.addEventListener('click', async () => {
+      await this.openImage(image.largeImageURL);
+    });
 
-   card.appendChild(imageElement);
-   return card;
-}
+    card.appendChild(imageElement);
+    return card;
+  }
+};
 
-export default { createImageCard, openImage };
+export default imageCard;
