2ac249ee5d589d38a2787d338fb989b7643ac38c
Dmytro Medvediev <dsisitkin@gmail.com>
2023-06-10 22:05:36 +0200

    update

---
+++ src/js/formatImageData.js
@@ -0,0 +1,12 @@
+
+export function formatImageData({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
+   return {
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads
+   };
+}
